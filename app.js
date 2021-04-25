da9c5cbadc4469f99ec423a885d093e3b159c6f5
denakof <denakofahi_21@yahoo.com>
2021-04-25 16:24:36 +0300

    updates

---
+++ app.js
@@ -0,0 +1,19 @@
+let userName = prompt("please enter your name?");
+ alert("welcome " + userName);
+document.write(`<span id="userName">welcome ${userName}</span>`);
+console.log(userName);
+let Age = prompt('how old are you?');
+alert(Age);
+console.log(Age);
+
+
+let package= prompt('which package do you want?1,2,3');
+document.write(package);
+console.log(package);
+
+let howmany = prompt("how many packages do you want?");
+alert("are you sure you want" + package + "package/s");
+document.write(package);
+console.log("number of packages="+ package);
+
+
