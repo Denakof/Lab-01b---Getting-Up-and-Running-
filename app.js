f2f14339a9516dd3020ae0162854e22cadced1a7
denakof <denakofahi_21@yahoo.com>
2021-05-11 02:09:31 +0300

    add

--- app.js
+++ app.js
@@ -9,22 +9,26 @@ let userName = prompt("please enter your name?");
  alert("welcome " + userName);
 document.write(`<span id="userName">welcome ${userName}</span>`);
 console.log(`please enter your name? `+ userName);
-
+alert("please enter your name?" + userName);
 
 let Age = prompt('how old are you?');
-alert(`your age is  ` + Age);
+alert(`how old are you?  ` + Age + "                     please enter your name?    " + userName);
 console.log(`how old are you?`+ Age);
+
 
 
 let package= prompt('which package do you want?1,2,3');
 document.write(package);
 console.log(`which package do you want?1,2,3`+ package);
-alert(`you selected   ` + package +`   package/s`)
+alert(` which package do you want?1,2,3 `+ package + `      how old are you?  ` + Age + "             please enter your name?    " + userName)
+
 
 let howmany = prompt("how many packages do you want?");
-alert("are you sure you want  " + howmany + "  package/s");
+alert("how many packages do you want?         " + howmany + "       which package do you want?1,2,3 "+ package + `  how old are you?    ` + Age + "             please enter your name?    " + userName);
 document.write(package);
 console.log(`how many packages do you want?`+ howmany);
 
+
+
 alert(`you ordered   `+howmany + `  package/s`);
-alert(`Thank you for trusting us`+ userName );
+alert(`Thank you for trusting us   `+ userName );
